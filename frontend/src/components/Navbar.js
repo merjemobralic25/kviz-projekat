ec24b88a689684f0e1efdbe4b4e046ecaefe6678
merjemobralic25 <merjem.obralic.25@size.ba>
2026-05-31 15:11:23 +0000

    navbaripriv

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../context/AuthContext';
 import toast from 'react-hot-toast';
 
 const Navbar = () => {
